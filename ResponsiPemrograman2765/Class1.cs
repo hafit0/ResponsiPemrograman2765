5ffc9331961bc0d6606af259519400648ee21e87
hafit0 <61793573+hafit0@users.noreply.github.com>
2020-04-14 12:05:22 +0700

    Add files via upload

---
+++ ResponsiPemrograman2765/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ResponsiPemrograman2765
+{
+    class Karyawan
+    {
+        public string Nama { get; set; }
+        public string Nik { get; set; }
+        public double  Gajibulanan { get; set; }
+
+        public Karyawan(string nik= "0", string nama="kosong",  double gajibulanan = 0)
+        {
+            Nama = nama;
+            Nik = nik;
+            if (gajibulanan < 0) { gajibulanan = 0; }
+            
+
+            Gajibulanan = gajibulanan;
+        }
+
+
+        public void getkaryawan1()
+        {
+            Console.WriteLine("1. \t {0} {1} \t {2}", Nik, Nama , Gajibulanan);
+        }
+        public void getkaryawan2()
+        {
+            Console.WriteLine("2. \t {0} {1} \t {2}", Nik, Nama, Gajibulanan);
+        }
+        public void getkaryawan3()
+        {
+            Console.WriteLine("1. \t {0} {1} \t {2}", Nik, Nama, Gajibulanan=Gajibulanan+(0.1*Gajibulanan));
+        }
+        public void getkaryawan4()
+        {
+            Console.WriteLine("2. \t {0} {1} \t {2}", Nik, Nama, Gajibulanan = Gajibulanan + (0.1 * Gajibulanan));
+        }
+    }
+}
---
+++ ResponsiPemrograman2765/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace ResponsiPemrograman2765
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("No \t Nik/Nama  \t Gajibulanan");
+            Console.WriteLine("-------------------------------------------");
+            Karyawan karyawan = new Karyawan( "190302123","Paijo", 3000000);
+            karyawan.getkaryawan1();
+            Karyawan karyawan2 = new Karyawan("190302124","jono", 2000000);
+            karyawan2.getkaryawan1();
+
+            Console.WriteLine("\n\nAsyiikk kenaikan gaji 10%\n");
+            Console.WriteLine("No \t Nik/Nama  \t Gajibulanan");
+            Console.WriteLine("-------------------------------------------");
+            karyawan.getkaryawan3();
+            karyawan2.getkaryawan4();
+
+
+            Console.ReadKey();
+        }
+    }
+}
